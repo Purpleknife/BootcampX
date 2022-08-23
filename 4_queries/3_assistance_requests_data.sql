571539258210c38dca6a775c5abeef3c89f7ce01
Hind <hind.nayhi@gmail.com>
2022-08-23 16:33:44 -0600

    Fix error: now it dipslays 26299 instead of 20299 rows.

--- 4_queries/3_assistance_requests_data.sql
+++ 4_queries/3_assistance_requests_data.sql
@@ -2,5 +2,5 @@ SELECT teachers.name AS teacher, students.name AS student, assignments.name AS a
 FROM assistance_requests
 JOIN teachers ON teachers.id = assistance_requests.teacher_id
 JOIN students ON students.id = assistance_requests.student_id
-JOIN assignments ON assignments.id = assistance_requests.assignment_id
+LEFT JOIN assignments ON assignments.id = assistance_requests.assignment_id
 ORDER BY duration;
